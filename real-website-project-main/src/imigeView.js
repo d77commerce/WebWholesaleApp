9b574588315827c97fb0416305318910a4f9d4a1
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-05-03 16:47:46 +0100

    new

--- real-website-project-main/src/imigeView.js
+++ real-website-project-main/src/imigeView.js
@@ -1,9 +1,27 @@
 import { icons } from './icons.js';
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+const overlay = document.querySelector('#overlay');
+const overlayDisplayPicture = document.querySelector('#displayViewPicture');
+const closeBtnView = document
+  .querySelector('#closeModal')
+  .addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
 
 export function openViewImege(iconImgInput) {
   icons.forEach(element => {
     if (element.img == iconImgInput) {
       console.log(element.img);
+      overlay.style.display = 'block';
+
+      const content = html`<h2>${element.iconName}</h2>
+        <img
+          src="./pictures/${element.img}.jpg"
+          alt="${element.iconName}"
+          width="500"
+          height="600"
+        />`;
+      render(content, overlayDisplayPicture);
     }
   });
 }
