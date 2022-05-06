347d7f4730bff86859dc289743631b4ba394d6bb
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-05-06 19:05:16 +0100

    update

--- src/orderList.js
+++ src/orderList.js
@@ -1,11 +1,10 @@
 export const orderItem = (itemName, itemSize, itemPieces) =>
-  `<p id="${itemName}remove">${itemName} size ${itemSize} £ ${itemPieces} </p><button id = "${itemName}remove" >remove X</button>`;
+  `<div id="${itemName}${itemSize}remove"><p>${itemName} size ${itemSize} £ ${itemPieces} </p><button name = "${itemName}${itemSize}remove" >remove X</button></div>`;
 
 const activRemove = document.querySelector('.All-total');
 activRemove.addEventListener('click', removeItem);
 function removeItem(ev) {
-  console.log(ev.target.id);
-  let item = ev.target.id;
-  let element = document.getElementById(item);
-  element.parentNode.removeChild(element);
+  console.log(ev.target.name);
+  let item = ev.target.name;
+  document.getElementById(item).remove();
 }
